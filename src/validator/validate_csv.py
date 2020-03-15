924bec726136b5dee2f4571b46be61bfc0cad808
Syed Yousuf <syed.yousuf@oohmedia.com.au>
2020-03-15 22:54:39 +1100

    Fixed recipes and ingredients df

--- src/validator/validate_csv.py
+++ src/validator/validate_csv.py
@@ -17,10 +17,10 @@ def validate_csv_data(df):
     is_quantity_correct = (df["Quantity"].fillna(0) % 2  >= 0).all()
 
     if(((df[df["Use By Date"] >= date_today]))['Item'].count() < 1):
-        return 'Nothing in the fridge to consume'
+        raise ValueError('Nothing in the fridge to consume')
     elif (is_measure_unit_correct is False):
-        return 'Invalid item measurement unit in the file'
+        raise ValueError('Invalid item measurement unit in the file')
     elif (is_quantity_correct is False):
-        return 'Invalid item quantity in the file'
+        raise ValueError('Invalid item quantity in the file')
     else:
         return df
--- what_to_cook.py
+++ what_to_cook.py
@@ -1,5 +1,5 @@
-import sys
 import pandas
+from pandas.io.json import json_normalize
 import time
 import pyspark as spark
 import pyspark.sql.functions as F
@@ -9,22 +9,43 @@ import json
 from src.validator.validate_csv import validate_csv_data
 
 
+def getRecipes(recipes_path):
+    try:
+        df = pandas.read_json(recipes_path, orient='records')
+    except:
+        print("Can't read recipes.")
+    
+    # print(validate_csv_data(ingredients))
+    recipes_exploded = df.explode('ingredients')
+    recipes_exploded_expanded = recipes_exploded.ingredients.apply(pandas.Series)
+    recipes=recipes_exploded.join(recipes_exploded_expanded)
+    recipes=recipes.drop('ingredients',1)
+    return (recipes.drop_duplicates(keep='first',inplace=False))
+
+def getIngredients(ingredients_path):
+    try:
+        ingredients = pandas.read_csv(ingredients_path)
+    except:
+        print("Can't read ingredients from CSV file.")
+    try:
+        ingredients_validated = validate_csv_data(ingredients)
+    except e:
+        print(e)
+    return ingredients_validated
+
+
 if __name__== "__main__":
     
     ingredients_path="./source_files/my-fridge.csv"
     recipes_path="./source_files/my-recipes.json"
-    try: 
-        ingredients = pandas.read_csv(ingredients_path)
-        recipes = pandas.read_json(recipes_path, orient='records')
-    except:
-        print("Can't read Fridge ingredients csv.")
     
-    # print(recipes.ingredients)
-    print(validate_csv_data(ingredients))
-    # print((recipes["ingredients"]))
-    # print("======================")
-    # recipes = recipes.join(recipes['ingredients'].apply(pandas.Series))
-    # print(recipes)
-    # print("======================")
+    ingredients=getIngredients(ingredients_path)
+    recipes=getRecipes(recipes_path)
+
     
-    # print(d[0])
+
+    print(ingredients)
+    print("================")
+    print(recipes)
+    print("================")
+    # result=pandas.merge()
